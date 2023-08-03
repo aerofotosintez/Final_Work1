9d1a5449a366c345b038ea34e5771bc761ab9770
aerofotosintez <aerofotosintez@gmail.com>
2023-08-03 13:37:18 +0300

    написал код

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//
+string[] M;
+Console.WriteLine("введите массив строк через запяую ");
+string stroka = Console.ReadLine();
+M = stroka.Split(',');
+var result = new string[M .Length];
+var realSize = 0;
+
+foreach(var value in M)
+{
+    if(value.Length <= 3)
+    result[realSize] = value;
+    realSize++;
+}
+Console.Write(string.Join(Environment.NewLine, result, 0, realSize));
+Console.ReadKey(true);
