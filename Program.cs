ee623d69821746bfb06c3c7e80da370971ce1c53
aerofotosintez <aerofotosintez@gmail.com>
2023-08-09 13:35:49 +0300

    навел красоту

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 ﻿//
-string[] M;
+string[] Massiv;
 Console.WriteLine("введите массив строк через запяую ");
 string stroka = Console.ReadLine();
-M = stroka.Split(',');
-var result = new string[M .Length];
+Massiv = stroka.Split(',');
+var result = new string[Massiv.Length];
 var realSize = 0;
 
-foreach(var value in M)
+foreach(var value in Massiv)
 {
     if(value.Length <= 3)
     result[realSize] = value;
